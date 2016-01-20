ee718fb4375ba3a9184fae013e8dd11d112de2b9
Dominick Madden <dominick.madden@gmail.com>
2016-01-20 20:40:46 +0100

    changes to event handling and animations

---
+++ gestureframework/src/main/java/hs_mannheim/gestureframework/animation/ElevateAndLeaveAnimation.java
@@ -0,0 +1,7 @@
+package hs_mannheim.gestureframework.animation;
+
+/**
+ * Created by Dome on 05.01.2016.
+ */
+public class ElevateAndLeaveAnimation {
+}
---
+++ gestureframework/src/main/java/hs_mannheim/gestureframework/gesture/swipe/SwipeMinDistanceConstraint.java
@@ -0,0 +1,7 @@
+package hs_mannheim.gestureframework.gesture.swipe;
+
+/**
+ * Created by Dome on 06.01.2016.
+ */
+public class SwipeMinDistanceConstraint {
+}

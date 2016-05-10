0f37a48b31c5528de8664e1f6d5c174dff8ec791
Dominick Madden <dominick.madden@gmail.com>
2016-05-10 11:38:26 +0200

    Cleans DoubleTapDetector class up a bit

--- gestureframework/src/main/java/hs_mannheim/gestureframework/gesture/doubletap/DoubleTapDetector.java
+++ gestureframework/src/main/java/hs_mannheim/gestureframework/gesture/doubletap/DoubleTapDetector.java
@@ -8,11 +8,11 @@ import hs_mannheim.gestureframework.gesture.GestureDetector;
 import hs_mannheim.gestureframework.model.IViewContext;
 
 public class DoubleTapDetector extends GestureDetector implements View.OnTouchListener {
-    private MotionEvent mCurrentDown, mLastDown, mCurrentUp, mLastUp;
-    private boolean mIsSecondTap, mMovedOutOfTapRegion;
+    private MotionEvent mLastDown, mLastUp;
+    private boolean mMovedOutOfTapRegion;
     private static final long MAX_TIME_DELTA = 300, MIN_TIME_DELTA = 50;
     private static final int MAX_DISTANCE = 150;
-    private static final String TAG = "DoubleTapDetector";
+    private static final String TAG = "[DoubleTapDetector]";
 
     /**
      * Detects a DoubleTap Gesture. Needs an {@link IViewContext} to listen for TouchEvents.
@@ -41,13 +41,12 @@ public class DoubleTapDetector extends GestureDetector implements View.OnTouchLi
                 break;
             case MotionEvent.ACTION_UP:
                 mLastUp = MotionEvent.obtain(motionEvent);
-
                 break;
             case MotionEvent.ACTION_MOVE:
                 int moveDistanceX = Math.abs((int) mLastDown.getX() - (int) motionEvent.getX());
                 int moveDistanceY = Math.abs((int) mLastDown.getY() - (int) motionEvent.getY());
 
-                if(moveDistanceX > MAX_DISTANCE || moveDistanceY > MAX_DISTANCE){
+                if (moveDistanceX > MAX_DISTANCE || moveDistanceY > MAX_DISTANCE) {
                     mMovedOutOfTapRegion = true;
                 }
                 break;
@@ -62,21 +61,19 @@ public class DoubleTapDetector extends GestureDetector implements View.OnTouchLi
             return false;
         }
 
-        Log.d(TAG, "Eventtimes: " + secondDown.getEventTime() + ", " + firstUp.getEventTime());
         final long deltaTime = secondDown.getEventTime() - firstUp.getEventTime();
         if (deltaTime > MAX_TIME_DELTA || deltaTime < MIN_TIME_DELTA) {
-            Log.d(TAG, "Tap timing bad! " + deltaTime);
+            Log.d(TAG, "Tap timing bad!" + deltaTime);
             return false;
         }
 
         int deltaX = Math.abs((int) firstDown.getX() - (int) secondDown.getX());
         int deltaY = Math.abs((int) firstDown.getY() - (int) secondDown.getY());
-        if(deltaX > MAX_DISTANCE || deltaY > MAX_DISTANCE){
-            Log.d(TAG, "Tap distance too big");
+        if (deltaX > MAX_DISTANCE || deltaY > MAX_DISTANCE) {
+            Log.d(TAG, "Tap distance too big!");
             return false;
         } else {
             return true;
         }
     }
-
 }
